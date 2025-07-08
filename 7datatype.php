0fe596d052579f77cb169d5cf713087c900398d1
ABHISHEK GUPTA <abhishekgupta1864@gmail.com>
2025-07-09 03:02:21 +0530

    first

---
+++ 7datatype.php
@@ -0,0 +1,21 @@
+<?php
+echo "This show the string type:<br/>";
+$name="Greater Noida is good city.";
+echo var_dump($name);
+
+echo "<br/>This show the integer type:<br/>";
+$num=10;
+echo var_dump($num);
+
+echo "<br/>This show the float type:<br/>";
+$float_num=1000.0012;
+echo var_dump($float_num);
+
+
+echo "<br/>This show the boolean type:<br/>";
+$boolean=true;
+echo var_dump($boolean);
+
+
+
+?>
